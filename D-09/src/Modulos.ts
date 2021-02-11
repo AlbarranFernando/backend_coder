6b71ade29b2934aa3f8c11d3d823b4b0b31dd863
AlbarranFernando <69657806+AlbarranFernando@users.noreply.github.com>
2021-02-11 03:39:10 -0300

    Desafio 09

---
+++ D-09/src/Modulos.ts
@@ -0,0 +1,56 @@
+  
+ class Modulos {
+
+     constructor(public products:any){}
+
+     addProduct(prod: any) {
+        const {title, price, thumbail} = prod
+        const id = (this.products.length + 1 ).toString()
+        const producto = {
+          id,
+          title,
+          price,
+          thumbail,
+        }
+        this.products.push(producto)
+     }
+
+     getProducts() {
+        let productoVista:any[] = this.products
+        if(!this.products.length) productoVista = [{error : "no hay productos cargados"}]
+         return productoVista;
+     }
+
+     findOneProduct(id: string){
+        let producto = this.products.find((prod:any) => prod.id === id);
+        if (!producto) producto = {error : 'producto no encontrado'}
+        return producto
+     }
+
+      updateProduct(prod: any, idn: string){
+         const {title, price, thumbail} = prod
+         const id = idn
+         const productoAct = {
+           id,
+           title,
+           price,
+           thumbail,
+         }
+         let producto = this.products.find((prod:any) => prod.id === id);
+         if (!producto) producto = {error : 'producto no encontrado'}
+            else  this.products.splice(this.products.indexOf(producto), 1,productoAct)
+
+
+         return productoAct
+    }
+
+      delProduct(id: string){
+      let producto = this.products.find((prod:any) => prod.id === id);
+      if (!producto) producto = {error : 'producto no encontrado'}
+         else  this.products.splice(this.products.indexOf(producto), 1)
+            
+      return producto
+    }
+ }
+ export default Modulos
+
